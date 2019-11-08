4bb0bcfc66268aa571419a65528d036d4844da64
Maureen <murimimaureen69@gmail.com>
2019-11-08 15:32:55 +0300

    add prototype

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Pizza(size, name) {
+    this.name = name;
+    this.size = size;
+}
+
+Pizza.prototype.addToArray = () => {
+    let arr = [];
+    this.size = {};
+    this.size.small = 500;
+    this.size.medium = 750;
+    this.size.large = 1000;
+
+}
