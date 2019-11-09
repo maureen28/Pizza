8ebcacd37eb58aacbd784b75228b10a5b6585f53
Maureen <murimimaureen69@gmail.com>
2019-11-09 19:46:14 +0300

    add paragraphs to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,47 @@
-function Pizza(size, name) {
+function Pizza(name) {
     this.name = name;
-    this.size = size;
+    this.size = [];
+    this.crust = [];
+    this.toppings = [];
 }
+// crust
+var glutenFree={
+    name:"glutenFree",price:120,
+}
+var stuffed={
+    name:"stuffed",price:80,}
+var crispy={
+    name:"crispy", price:100,
+}
+function Size(large,small,medium){
+this.large=large
+this.medium=medium
+this.small=small
+}
+function Toppings(bacon,pepper,extra_cheese){
+    this.bacon=bacon
+    this.pepper=pepper
+    this.extra_cheese=extra_cheese
+}
+function Crust(stuffed,crispy,glutenFree){
+    this.stuffed=stuffed
+    this.glutenFree=glutenFree
+    this.crispy=crispy
+    }
 
-Pizza.prototype.addToArray = () => {
-    let arr = [];
-    this.size = {};
-    this.size.small = 500;
-    this.size.medium = 750;
-    this.size.large = 1000;
-for(let k in this.size){
-    return this.size
-}
-}
+var hawaiannPizza={name:"Hawaiian Pizza", properties: [size,crust,toppings]}
+var meatLovers={name:"meatLovers", properties: [size,crust,toppings]}
+var vegan={name:"vegan", properties: [size,crust,toppings]}
+var magharitta={name:"magharitta", properties: [size,crust,toppings]}
+
+//jQuery
+$(document).ready(function(event){
+    event.preventDefault()
+    $("#o").click(function(){
+
+    $("#xx").val()
+    $("#xx option:selected").text()
+    });
+});
+
+
