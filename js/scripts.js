9b1259eaf3af4cab62d2983c4bf70bebde2350fe
Maureen <murimimaureen69@gmail.com>
2019-11-11 10:14:05 +0300

    adding business logic js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,53 @@
-function Pizza(name) {
-    this.name = name;
-    this.size = [];
-    this.crust = [];
-    this.toppings = [];
-}
-// Pizza(names)
-function HawaiannPizza{
-    this.name = "Hawaiann";
+function Pizza{
+    this.name ="name";
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
 }
-hawaiannPizza.prototype=function(){
+
+// crust
+Pizza.prototype.Crust = function() { 
+    if(crust==="glutenFree"){
+        crust=120
+    }else if(crust==="stuffed"){
+        crust=100
+    } else if(crust==="crispy"){
+        crust=80
+}
+return crust;
+}
+
+// size
+Pizza.prototype.size = function() {
+    if(size==="small"){
+        size=500
+    }else if(size==="medium"){
+        size=750
+    } else if(size==="large"){
+        size=1000
+}
+return size;
+}
+
+// toppings
+Pizza.prototype.toppings = function() {
+    if(toppings==="bacon"){
+        toppings=50
+    }else if(toppings==="extraCheese"){
+        toppings=40
+    } else if(toppings==="pepper"){
+        size=30
+    }
+    return toppings;
+}
+    // Pizza(names)
+function Pizza{
+    this.name = "name";
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+}
+Pizza.prototype.Toppings=function(){
     return"Name"+""+"Size"+""+"Crust"+""+"Toppings"
 }
 
@@ -44,43 +80,6 @@ function VeganPizza{
 VeganPizza.prototype=function(){
     return"Name"+""+"Size"+""+"Crust"+""+"Toppings"
 }
-
-// crust
-function Crust(){
-    var crust;
-    if(crust==="glutenFree"){
-        crust=120
-    }else if(crust==="stuffed"){
-        crust=100
-    } else if(crust==="crispy"){
-        crust=80
-}
-return crust
-
-// size
-function Size(){
-    var size;
-    if(size==="small"){
-        size=500
-    }else if(size==="medium"){
-        size=750
-    } else if(size==="large"){
-        size=1000
-}
-return size
-
-// toppings
-function Toppings(){
-    var toppings;
-    if(toppings==="bacon"){
-        toppings=50
-    }else if(toppings==="extraCheese"){
-        toppings=40
-    } else if(toppings==="pepper"){
-        size=30
-    }
-    return toppings
-
 
 /*var hawaiannPizza={name:"Hawaiian Pizza", properties: [size,crust,toppings]}
 var meatLovers={name:"meatLovers", properties: [size,crust,toppings]}
