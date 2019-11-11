d8968bad3ec3a539017c544cd1b22819e0b23d1e
Maureen <murimimaureen69@gmail.com>
2019-11-11 14:28:39 +0300

    add container-fluid and style it on css

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function Pizza(name) {
     this.size = [];
     this.crust = [];
     this.toppings = [];
+    this.number=[];
 }
 // Pizza(names)
 function HawaiannPizza(){
@@ -11,6 +12,7 @@ function HawaiannPizza(){
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.number=number;
 }
 hawaiannPizza.prototype = function () {
     return "Name" + "" + "Size" + "" + "Crust" + "" + "Toppings"
@@ -20,6 +22,7 @@ function MediterraneanPizza(){
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.number=number;
 }
 MediterraneanPizza.prototype = function () {
     return "Name" + "" + "Size" + "" + "Crust" + "" + "Toppings"
@@ -29,6 +32,7 @@ function MeatLoversPizza(){
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.number=number;
 }
 MeatLoversPizza.prototype = function () {
     return "Name" + "" + "Size" + "" + "Crust" + "" + "Toppings"
@@ -38,6 +42,7 @@ function VeganPizza(){
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.number=number;
 }
 VeganPizza.prototype = function () {
     return "Name" + "" + "Size" + "" + "Crust" + "" + "Toppings"
@@ -52,8 +57,8 @@ function Crust() {
     } else if (crust === "crispy") {
         crust = 80
     }
-    return crust
-
+    return crust;
+}
     // size
     function Size() {
         var size;
@@ -64,20 +69,19 @@ function Crust() {
         } else if (size === "large") {
             size = 1000
         }
+        return size;
+    }
         // toppings
         function Toppings() {
             var toppings;
             if (toppings === "bacon") {
                 toppings = 50
             } else if (toppings === "extra-cheese") {
-                size = 60
+                toppings = 60
             } else if (size === "pepper") {
-                size = 40
-
-                /*var hawaiannPizza={name:"Hawaiian Pizza", properties: [size,crust,toppings]}
-                var meatLovers={name:"meatLovers", properties: [size,crust,toppings]}
-                var vegan={name:"vegan", properties: [size,crust,toppings]}
-                var magharitta={name:"magharitta", properties: [size,crust,toppings]}*/
+                toppings = 40
+            }   return toppings;
+        }
 
                 //jQuery
                 //meatLovers
@@ -100,6 +104,12 @@ function Crust() {
                             $("#checkout").text(toppings)
                         })
                     });
+                    $("#btn-addtocart-ml").click(function () {
+                    $(".input").change(function () {
+                        size = $(".input option:selected").val();
+                        $("#checkout").text(input)
+                    })
+                });
                     //meditarrenean
                     $(document).ready(function () {
                         $("#btn-addtocart-md").click(function () {
@@ -118,6 +128,12 @@ function Crust() {
                             $(".toppings").change(function () {
                                 size = $(".toppings option:selected").val();
                                 $("#checkout").text(toppings)
+                            })
+                        });
+                        $("#btn-addtocart-md").click(function () {
+                            $(".input").change(function () {
+                                size = $(".input option:selected").val();
+                                $("#checkout").text(input)
                             })
                         });
                         // hawaiian
@@ -140,6 +156,12 @@ function Crust() {
                                     $("#checkout").text(toppings)
                                 })
                             });
+                            $("#btn-addtocart-hawaii").click(function () {
+                            $(".input").change(function () {
+                                size = $(".input option:selected").val();
+                                $("#checkout").text(input)
+                            })
+                        });
                             //vegan
                             $(document).ready(function () {
                                 $("#btn-addtocart-vegan").click(function () {
@@ -160,4 +182,10 @@ function Crust() {
                                         $("#checkout").text(toppings)
                                     })
                                 });
-                            }
+                                $("#btn-addtocart-vegan").click(function () {
+                                    $(".input").change(function () {
+                                        size = $(".input option:selected").val();
+                                        $("#checkout").text(input)
+                                    })
+                                });
+        
